722a8fb9999de67dcee625e0009f8129bce95f32
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-01-09 20:14:12 +0400

    feature/1.0/2: home page progress

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 // Load environment variables
 dotenv.config({ path: './config/config.env' });
@@ -7,11 +8,39 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 
 const PORT = process.env.PORT;
+const API_BASE = process.env.API_BASE;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
+app.get('/', async (req, res) => {
+  let nbPages;
+  let currentPage = 1;
+  let order = 'ratingDesc';
+  let books = [];
+
+  try {
+    const result = await axios.get(`${API_BASE}/pages/nb`);
+    nbPages = result.data.nbPages;
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (req.query.p) {
+    currentPage = req.query.p;
+  }
+
+  if (req.query.order) {
+    order = req.query.order;
+  }
+
+  try {
+    const result = await axios.get(`${API_BASE}/books/all`, { params: { currentPage, order } });
+    books = result.data;
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.render('index.ejs', { nbPages, currentPage, order, books });
 });
 
 app.listen(PORT, () => {
